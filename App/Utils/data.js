e96f8ab91a812c83eaa66be4eefdc2a182a6a3f8
Mukesh Jha <mukeshjha@Ethan-MacBook-Pro.local>
2022-07-13 21:55:49 +0530

    initial ios working setup

---
+++ App/Utils/data.js
@@ -0,0 +1,82 @@
+export const data = [
+  {
+    id: 1,
+    post: {
+      url: require('../Assets/video1.mov'),
+      url_: "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
+    },
+    user: {
+      userId: 1,
+      name: 'Kalki Patel',
+      isFollowing: false,
+    },
+    comment: 101,
+    like: '121.3 k',
+    likeStatus: true,
+    postText: 'React Native reels with actions shown in this project',
+  },
+  {
+    id: 2,
+    post: {
+      url: require('../Assets/video2.mov'),
+      url_: "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4",
+    },
+    user: {
+      userId: 2,
+      name: 'Kajol Patel',
+      isFollowing: true,
+    },
+    comment: 100,
+    like: '10.3 k',
+    likeStatus: false,
+    postText: 'React Native reels with actions shown in this project',
+  },
+  {
+    id: 3,
+    post: {
+      url: require('../Assets/video3.mov'),
+      url_: "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerEscapes.mp4",
+    },
+    user: {
+      userId: 3,
+      name: 'Kalki Patel',
+      isFollowing: false,
+    },
+    comment: 10,
+    like: '121',
+    likeStatus: true,
+    postText: 'React Native reels with actions shown in this project',
+  },
+  {
+    id: 4,
+    post: {
+      url: require('../Assets/video4.mov'),
+      url_: "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerFun.mp4",
+    },
+    user: {
+      userId: 4,
+      name: 'Kajol Patel',
+      isFollowing: true,
+    },
+    comment: 300,
+    like: '150.3 k',
+    likeStatus: false,
+    postText: 'React Native reels with actions shown in this project',
+  },
+  {
+    id: 5,
+    post: {
+      url: require('../Assets/video5.mov'),
+      url_: "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4",
+    },
+    user: {
+      userId: 5,
+      name: 'Kalki Patel',
+      isFollowing: false,
+    },
+    comment: 320,
+    like: '121.3 k',
+    likeStatus: true,
+    postText: 'React Native reels with actions shown in this project',
+  },
+];
